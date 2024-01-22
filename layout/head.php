58fc343fb82f1a39925113e3fcb509a610635dc1
szabomarcel <szabomarcell34@gmail.com>
2024-01-22 21:03:55 +0100

    remek

--- layout/head.php
+++ layout/head.php
@@ -3,7 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Football</title>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- Bootstrap CSS -->
     <!--<link href="path/to/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     Bootstrap JS (popper.js is required for dropdowns) -->
--- login/navbar.php
+++ login/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary"> <!--fixed-top-->
     <div class="container-fluid">
     <a class="navbar-brand" href="index.php?menuItem=fooldal"><img src="kepek/pele-focilabda.svg" alt="pele-focilabda.svg" style="width: 30px; height: 30px;"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse navbar-dark bg-secondary   " id="navbarNav">
@@ -40,33 +40,51 @@
         </div>    
     </div>
 </nav>    
-<div class="navbar-wrapper">
-      <div class="container">
-
-        <nav class="navbar navbar-inverse navbar-static-top">
-          <div class="container">
+<nav class="navbar-wrapper">
+    <div class="navbar navbar-collapsed-sm navbar-fixed-top navbar-default" role="navigation">
+        <div class="container" >
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                    <li><a href="">Page 1</a>
-                    </li>
-                    <li><a href="">Page 2</a>
-                    </li>
-                    <li><a href="">Page 3</a>
-                    </li>
-
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-      </div>
-    </div>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div> <!-- navbar-header -->
+            <nav class="navbar-collapse navbar-header-collapse collapse">
+                <ul class="nav navbar-nav navbar-right ">
+                <?php
+                    echo '<li class="nav-item navbar navbar-light bg-light">
+                            <a class="nav-link'. ($menuItem == 'felhasznalo'?' active ': '') .'"href="index.php?menuItem=fooldal">Főoldal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar navbar-light bg-light'. ($menuItem == 'atigazolas'?' active ': '') .'" href="index.php?menuItem=atigazolas">Átigazolási hírek</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar navbar-light bg-light'. ($menuItem == 'csapatok'?' active ': '') .'" href="index.php?menuItem=csapatok">Csapatainkról</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar navbar-light bg-light'. ($menuItem == 'foci'?' active ': '') .'" href="index.php?menuItem=foci">Mini Game</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar navbar-light bg-light'. ($menuItem == 'videokepek'?' active ': '') .'" href="index.php?menuItem=videokepek">Videók és képek</a>
+                        </li>
+                        <div class="dropdown">
+                            <a data-mdb-dropdown-init class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuAvatar" role="button" aria-expanded="false">
+                                <img src="kepek/navbar/man.png" class="rounded-circle" height="25" alt="Black and White Portrait of a Man" loading="lazy"/>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                                <li class="nav-item">
+                                    <a class="nav-link navbar navbar-black bg-light' . ($menuItem == 'felhasznalo'?' active' : '') .'"href="index.php?menuItem=felhasznalo">Profilom <img src="kepek/navbar/profile.png" height="25" alt="Black and White Portrait of a Man" loading="lazy"/></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link navbar navbar-light bg-light' . ($menuItem == 'logout'?' active' : '') .'"href="index.php?menuItem=logout">Kijelentkezés <img src="kepek/navbar/logout.png" height="25" alt="Black and White Portrait of a Man" loading="lazy"/></a>
+                                </li>
+                            </ul>
+                        </div>';
+                ?>				      
+                </ul> <!-- nav -->
+            </nav> <!-- navbar collapse -->
+        </div> <!-- container -->
+    </div> <!-- navbar -->
+</nav> <!-- navbar-wrapper -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
