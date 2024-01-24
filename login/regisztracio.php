64ae05185aa1e20d5a665ba246cb07b94e4f604c
szabomarcel <szabomarcell34@gmail.com>
2024-01-24 10:57:17 +0100

    remek

--- login/regisztracio.php
+++ login/regisztracio.php
@@ -148,8 +148,3 @@
     </div>
   </div>
 </section>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
