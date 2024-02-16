554915013f347487fd228b2f8550f42ca741606b
szabomarcel <szabomarcell34@gmail.com>
2024-02-16 14:32:08 +0100

    remek

---
+++ login/csapatok.php
@@ -0,0 +1,410 @@
+ <!-- ======= magyarlabdarugas ======= -->
+ <section id="magyarlabdarugas" class="d-flex align-items-center">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+      <h1>Ezen az oldalon mindent megtalálsz amit tudni szeretnél a Magyar futballal kapcsolatban</h1>
+    </div>
+    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+      <img src="kepek/mlsz.png" class="animated" alt="" id="mlsz">
+    </div>
+  </div>
+</div>
+</section><!-- End magyarlabdarugas -->
+<!-- ======= szponzorok ======= -->
+<section id="szponzorok" class="szponzorok section-bg"> 
+  <div class="container">
+    <div class="row" data-aos="zoom-in">
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/hell.png" id="szponzorlogok" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/mastergood.png" id="szponzorlogok" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/meszaros.png" id="szponzorlogok" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/thome.png" id="szponzorlogok" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/tippmix.png" id="szponzorlogok" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="kepek/szponzorok/zsory.png" id="szponzorlogok" alt="">
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Szponzorok Section -->
+<!-- ======= Kviz ======= -->
+<section id="kviz" class="kviz section-bg">
+  <div class="section-title" id="kvizcim">
+    <h2>Napi Kvíz</h2>
+  </div>
+  <div class="container">
+    <div class="controls">
+      <button id="start-btn" class="start-btn btn">Kezdés</button>
+      <button id="next-btn" class="next-btn btn hide">Következő kérdés</button>
+    </div>
+    <div id="question-container" class="hide">
+      <div id="question">kerdes</div>
+    <div id="answer-buttons" class="btn-grid">             
+          <button id="btnt" class="btn">válasz 1</button>
+          <button id="btnt" class="btn">válasz 2</button>
+          <button id="btnt" class="btn">válasz 3</button>
+          <button id="btnt" class="btn">válasz 4</button>
+    </div>
+  </div>
+</section><!-- End Kviz Section -->
+<!-- ======= NB1 tabella ======= -->
+<section id="tabella" class="tabella section-bg">
+  <div class="section-title" id="tabellacim">
+    <h2>A Labdarúgó NB1 Tabellája</h2>
+  </div>
+  <div class="container" id="tabellacontainer">
+    <table class="styled-table" id="tabellatabla">
+      <thead> 
+        <tr>
+          <th>#</th>  
+          <th>CSAPAT</th>
+          <th>LM</th>
+          <th>GY</th>
+          <th>D</th>
+          <th>V</th>
+          <th>PONTOK</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="active-row">
+          <td>1</td>  
+          <td>
+            <img id="csapatok" src="kepek/csapatok/PFC.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>10</td>
+          <td>4</td>
+          <td>2</td>
+          <td>34</td>
+        </tr>
+        <tr>
+          <td>2</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/FTC.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>9</td>
+          <td>3</td>
+          <td>4</td>
+          <td>30</td>
+        </tr>
+        <tr>
+          <td>3</td> 
+          <td><img id="csapatok"  src="kepek/csapatok/Fehervar.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>8</td>
+          <td>3</td>
+          <td>5</td>
+          <td>27</td>
+        </tr>
+        <tr>
+          <td>4</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/PAFC.png" height=60 width=90></img>          
+          </td>
+          <td>16</td>
+          <td>7</td>
+          <td>6</td>
+          <td>3</td>
+          <td>27</td>
+        </tr>
+        <tr>
+          <td>5</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/DVSC.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>7</td>
+          <td>3</td>
+          <td>6</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <td>6</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/MTK.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>7</td>
+          <td>3</td>
+          <td>6</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <td>7</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/DVTK.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>7</td>
+          <td>2</td>
+          <td>7</td>
+          <td>23</td>
+        </tr>
+        <tr>
+          <td>8</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/UTE.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>6</td>
+          <td>3</td>
+          <td>7</td>
+          <td>21</td>
+        </tr>
+        <tr>
+          <td>9</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/KTE.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>6</td>
+          <td>2</td>
+          <td>8</td>
+          <td>20</td>
+        </tr>
+        <tr>
+          <td>10</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/zte.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>4</td>
+          <td>3</td>
+          <td>9</td>
+          <td>15</td>
+        </tr>
+        <tr id="kiesohely">
+          <td>11</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/kovesd.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>4</td>
+          <td>2</td>
+          <td>10</td>
+          <td>14</td>
+        </tr>
+        <tr id="kiesohely">
+          <td>12</td>  
+          <td><img id="csapatok"  src="kepek/csapatok/kisvarda.png" height=60 width=90></img>
+          </td>
+          <td>16</td>
+          <td>3</td>
+          <td>2</td>
+          <td>11</td>
+          <td>11</td>
+        </tr>
+      </tbody>
+    </table>
+</div>              
+</section><!-- End NB1 tabella Section -->
+    <!-- ======= Gollovolista ======= -->
+    <section id="gollovolista" class="gollovolista">
+      <div class="section-title" id="gollovolistacim">
+        <h2>A Labdarúgó NB1 Góllövőlistája</h2>
+      </div>
+      <div class="container" id="tabellacontainer">
+        <table class="styled-table" id="gollovotabla">
+          <thead> 
+            <tr>
+              <th>#</th>  
+              <th>Név</th>
+              <th>Csapat</th>
+              <th>Nemzetiség</th>
+              <th>LM</th>
+              <th>G</th>
+              <th>GP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="active-row">
+              <td>1.</td>  
+              <td>Varga B.</td> 
+              <td>Ferencvárosi TC</td>
+              <td>Magyar</td>
+              <td>8</td>
+              <td>8</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>2.</td>  
+              <td>Drazic S.</td> 
+              <td>Mezőkövesd</td>
+              <td>Szerb</td>
+              <td>8</td>
+              <td>6</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>3.</td> 
+              <td>Ljujic M.</td> 
+              <td>Újpest</td>
+              <td>Szerb</td>
+              <td>9</td>
+              <td>5</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>4.</td>  
+              <td>Plsek J.</td> 
+              <td>Puskás Akadémia</td>
+              <td>Cseh</td>
+              <td>9</td>
+              <td>5</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>5.</td>  
+              <td>Bognár I.</td> 
+              <td>MTK Budapest</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>4</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>5.</td>  
+              <td>Németh K.</td> 
+              <td>MTK Budapest</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>4</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>7.</td>  
+              <td>Traore A</td> 
+              <td>Ferencvárosi TC</td>
+              <td>Mali</td>
+              <td>8</td>
+              <td>4</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>8.</td>  
+              <td>Kodro K.</td> 
+              <td>Fehérvár FC</td>
+              <td>Bosznia</td>
+              <td>9</td>
+              <td>4</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>8.</td>  
+              <td>Loncar S.</td> 
+              <td>Debreceni VSC</td>
+              <td>Montenegró</td>
+              <td>9</td>
+              <td>4</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>10.</td>  
+              <td>Kalmár Zs.</td> 
+              <td>Fehérvár FC</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>3</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>11.</td>  
+              <td>Horváth K.</td> 
+              <td>Kecskeméti TE</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>3</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>11.</td> 
+              <td>Morschel H.</td> 
+              <td>Újpest</td>
+              <td>Dominika</td>
+              <td>9</td>
+              <td>3</td>
+              <td>1</td>
+            </tr>
+            <td>13.</td>  
+              <td>Ambrose P.</td> 
+              <td>Újpest</td>
+              <td>Nigéria</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>13.</td>  
+              <td>Costa Martins J. M.</td> 
+              <td>Ferencvárosi TC</td>
+              <td>Brazil</td>
+              <td>8</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>13.</td> 
+              <td>Edomwonyi B.</td> 
+              <td>Diósgyőri VTK</td>
+              <td>Nigéria</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <td>13.</td>  
+              <td>Katona M.</td> 
+              <td>Fehérvár FC</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>13.</td>  
+              <td>Könyves N.</td> 
+              <td>Paks</td>
+              <td>Magyar</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>13.</td> 
+              <td>Lisztes K.</td> 
+              <td>Ferencvárosi TC</td>
+              <td>Magyar</td>
+              <td>8</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <td>13.</td>  
+              <td>Mance A.</td> 
+              <td>Zalaegerszeg</td>
+              <td>Horvát</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>13.</td>  
+              <td>Mesanovic J.</td> 
+              <td>Kisvárda</td>
+              <td>Bosznia</td>
+              <td>9</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>  
+    </section><!-- End Gollovolista Section -->
+</main><!-- End #main -->
--- web/main.js
+++ web/main.js
@@ -66,3 +66,221 @@ function addComment() {
         alert("A comment cannot be empty.");
     }
 }
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let currentQuestionIndex // Már nem szükséges a shuffledQuestions vagy a setNextQuestion függvény
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  showQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  currentQuestionIndex = 0 // Csak az aktuális kérdés indexének nullázása szükséges
+  score = 0
+  questionContainerElement.classList.remove('hide')
+  showQuestion() // Mindjárt meghívjuk, hogy megjelenítse az első kérdést
+}
+
+function showQuestion() {
+  resetState()
+  const currentQuestion = questions[currentQuestionIndex] // Az aktuális kérdést kiválasztjuk az index alapján
+  questionElement.innerText = currentQuestion.question
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (questions.length > currentQuestionIndex + 1) { // Ellenőrizzük, hogy van-e még további kérdés
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Újra kitöltöm'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+function updateScore() {
+    scoreDisplay.innerText = `Pontszám: ${score}` // Frissítsük a pontszám kijelzést
+  }
+  
+
+const questions = [
+  {
+    question: '1. Hány gólt szerzett eddig ifj Lisztes krisztián a Ferencváros csapatában?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '7', correct: true },
+      { text: '8', correct: false },
+      { text: '10', correct: false }
+    ]
+  },
+  {
+    question: '2. Hány KTE futballista Magyar válogatott jelenleg?',
+    answers: [
+      { text: '5', correct: false },
+      { text: '4', correct: false },
+      { text: '1', correct: false },
+      { text: '3', correct: true }
+    ]
+  },
+  {
+    question: '3. Hanyadik selejtező körig jutott el a DVSC a legutóbbi Konferencia liga kiírásban?',
+    answers: [
+      { text: '3', correct: true },
+      { text: '5', correct: false },
+      { text: '2', correct: false },
+      { text: '1', correct: false }
+    ]
+  },
+  {
+    question: '4. Ki az Újpest FC házi gólkirálya jelenleg?',
+    answers: [
+      { text: 'Peter Ambrose', correct: false },
+      { text: 'Csoboth Kevin', correct: false },
+      { text: 'Matija Ljujic', correct: true },
+      { text: 'Heinz Morschel', correct: false }
+    ]
+  },
+  {
+    question: '5. A bajnokság hanyadik helyen végzett az előző szezonban a Puskás Akadémia?',
+    answers: [
+      { text: 'negyedik', correct: true },
+      { text: 'ötödik', correct: false },
+      { text: 'hetedik', correct: false },
+      { text: 'második', correct: false }
+    ]
+  },
+  {
+    question: '6. Az NB1 előző szezonjában melyik két csapat esett ki?',
+    answers: [
+      { text: 'Budapest Honvéd/Kisvárda', correct: false },
+      { text: 'Vasas/Mezőkövesd', correct: false },
+      { text: 'Budapest Honvéd/Vasas', correct: true },
+      { text: 'ZTE/Fehérvár FC', correct: false }
+    ]
+  },
+  {
+    question: '7. Hány mérkőzést nyert meg a Vasas tavaly az NB1-ben?',
+    answers: [
+      { text: '5', correct: false },
+      { text: '4', correct: true },
+      { text: '7', correct: false },
+      { text: '10', correct: false }
+    ]
+  },
+  {
+    question: '8. Melyik csapat nyerte tavaly a Mol Magyar kupát?',
+    answers: [
+      { text: 'ZTE', correct: true },
+      { text: 'Ferencvárosi TC', correct: false },
+      { text: 'Budafok', correct: false },
+      { text: 'Újpest FC', correct: false }
+    ]
+  },
+  {
+    question: '9. Hány külföldi játékos szerepel a Paksi FC csapatában?',
+    answers: [
+      { text: '2', correct: false },
+      { text: '5', correct: false },
+      { text: '3', correct: false },
+      { text: '0', correct: true }
+    ]
+  },
+  {
+    question: '10. Mennyit fizetett a Liverpool Szoboszlai Dominikért a nyáron?',
+    answers: [
+      { text: '35 millió eurót', correct: false },
+      { text: '50 millió eurót', correct: false },
+      { text: '80 millió eurót', correct: true },
+      { text: '70 millió eurót', correct: false }
+    ]
+  },
+  {
+    question: '11. Melyik Angol csapatba igazolt nyáron Kerkez Milos',
+    answers: [
+      { text: 'Bournemouth', correct: true },
+      { text: 'Wolverhampton', correct: false },
+      { text: 'Leicester City', correct: false},
+      { text: 'Fulham', correct: false }
+    ]
+  },
+  {
+    question: '12. Melyik bajnokságban futballozik jelenleg Vancsa Zalán?',
+    answers: [
+      { text: 'Holland Bajnokság', correct: false },
+      { text: 'Belga Bajnokság', correct: true },
+      { text: 'Norvég Bajnokság', correct: false },
+      { text: 'Lengyel Bajnokság', correct: false }
+    ]
+  },
+  {
+    question: '13. Ki volt az NB1 gólkirálya a tavalyi szezonban?',
+    answers: [
+      { text: 'Horváth Krisztofer', correct: false },
+      { text: 'Dorian Babunski', correct: false },
+      { text: 'Adama Traore', correct: false },
+      { text: 'Varga Barnabás', correct: true }
+    ]
+  },
+  {
+    question: '14. Melyik két csapat jutott fel újoncként az NB1-be 2023 nyarán?',
+    answers: [
+      { text: 'DVTK/Győri Eto', correct: false },
+      { text: 'Nyíregyháza/Szeged', correct: false },
+      { text: 'DVTK/MTK', correct: true },
+      { text: 'MTK/Haladás FC', correct: false }
+    ]
+  },
+  {
+    question: '15. Melyik az NB1 legnagyobb stadionja?',
+    answers: [
+      { text: 'Groupama Aréna(Ferencváros)', correct: true },
+      { text: 'Nagyerdei Stadion(Debrecen)', correct: false },
+      { text: 'MOl Aréna Sóstó(Fehérvár FC)', correct: false },
+      { text: 'Pancho Aréna(Puskás Akadémia)', correct: false }
+    ]
+  },
+]
