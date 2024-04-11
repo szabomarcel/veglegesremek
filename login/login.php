428f4a3dd78eeb6fa092e953a322b08bba56b32f
szabomarcel <szabomarcell34@gmail.com>
2024-04-11 14:42:29 +0200

    remek

--- login/login.php
+++ login/login.php
@@ -18,53 +18,48 @@ if (filter_input(INPUT_POST,'belepesiAdatok',FILTER_VALIDATE_BOOLEAN,FILTER_NULL
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-          <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-          <button type="button" class="btn btn-primary btn-floating mx-1">
-            <i class="fa fa-facebook-f"></i>
-          </button>
+          <p class="lead fw-normal mb-0 me-3">Jelentkezen be</p>
+          <a href="https://www.facebook.com/?locale=hu_HU">
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fa fa-facebook-f"></i>
+            </button>
+          </a>
           
-          <button type="button" class="btn btn-primary btn-floating mx-1">
-            <i class="fa fa-twitter"></i>
-          </button>
+          <a href="https://twitter.com/?lang=hu">
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fa fa-twitter"></i>
+            </button>
+          </a>
           
-          <button type="button" class="btn btn-primary btn-floating mx-1">
-            <i class="fa fa-linkedin"></i>
-          </button>
+          <a href="https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=ARZ0qKKJxwn0tYHfIeuWDTqt0K9Nh14VT91BRgYtF8thCXDGqd5L3m0uPicXMAze8GheSU9U4CK8vw&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fa fa-google"></i>
+            </button>
+          </a>
         </div>
         
         <form action="#" method="post">
           <div class="divider d-flex align-items-center my-4">
-            <p class="text-center fw-bold mx-3 mb-0">Or</p>
+            <p class="text-center fw-bold mx-3 mb-0">Vagy</p>
           </div>
 
           <!-- Felhasználói név bemenet -->
           <div class="form-outline mb-4">
             <label class="form-label" for="name" aria-describedby="nameHelp" autocomplete="name" >Felhasználói név: </label>
             <input type="text" id="name" name="name" class="form-control form-control-lg" required
-              placeholder="Enter a valid name address" />
+              placeholder="Adjon meg egy érvényes névcímet" />
           </div>
 
           <!-- Jelszó bemenet -->
           <div class="form-outline mb-3">
             <label class="form-label" for="password"  autocomplete="address-line1">Jelszó: </label>
             <input type="password" id="password" name="password" class="form-control form-control-lg" required
-              placeholder="Enter password" />
-          </div>
-
-          <div class="d-flex justify-content-between align-items-center">
-            <!-- Checkbox -->
-            <!--<div class="form-check mb-0">
-              <input class="form-check-input me-2" type="checkbox" value="true" id="form2Example3" />
-              <label class="form-check-label" for="form2Example3">
-                Remember me
-              </label>
-            </div>-->
-            <a href="#!" class="text-body">Forgot password?</a>
-          </div>
+              placeholder="Írja be a jelszót" />
+          </div>          
 
           <div class="text-center text-lg-start mt-4 pt-2">
-            <button type="submit" class="btn btn-primary btn-lg" name="belepesiAdatok" style="padding-left: 2.5rem; padding-right: 2.5rem;" value="true">Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="index.php?register=register" class="link-danger">Register</a></p>
+            <button type="submit" class="btn btn-primary btn-lg" name="belepesiAdatok" style="padding-left: 2.5rem; padding-right: 2.5rem;" value="true">Bejelentkezés</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Nincs porfilod? <a href="index.php?register=register" class="link-danger">Regisztrálj</a></p>
           </div>
         </form>
       </div>
@@ -80,17 +75,14 @@ if (filter_input(INPUT_POST,'belepesiAdatok',FILTER_VALIDATE_BOOLEAN,FILTER_NULL
 
     <!-- Right -->
     <div>
-      <a href="#!" class="text-white me-4">
+      <a href="https://www.facebook.com/?locale=hu_HU" class="text-white me-4">
         <i class="fa fa-facebook-f"></i>
       </a>
-      <a href="#!" class="text-white me-4">
+      <a href="https://twitter.com/?lang=hu" class="text-white me-4">
         <i class="fa fa-twitter"></i>
       </a>
-      <a href="#!" class="text-white me-4">
+      <a href="https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=ARZ0qKKJxwn0tYHfIeuWDTqt0K9Nh14VT91BRgYtF8thCXDGqd5L3m0uPicXMAze8GheSU9U4CK8vw&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin" class="text-white me-4">
         <i class="fa fa-google"></i>
-      </a>
-      <a href="#!" class="text-white">
-        <i class="fab fa-linkedin-in"></i>
       </a>
     </div>
     <!-- Right -->
